7b9f6f7a903495050b6f8fd3e429861c41d7a20d
bendikjohansen <bendikj@gmail.com>
2021-07-25 22:59:54 +0200

    Remove projectiles on screen exited

--- Common/Projectile.gd
+++ Common/Projectile.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 export(float) var SPEED = 1000
 var direction = Vector2.RIGHT
@@ -6,4 +6,11 @@ var direction = Vector2.RIGHT
 onready var velocity = direction.normalized() * SPEED
 
 func _physics_process(delta):
-	move_and_slide(velocity)
+	position += velocity * delta
+
+	if get_overlapping_bodies():
+		queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
