9e75c0e5d5ac3fb4b92fb6fb0df07068c522a054
Bendik Mathias Johansen <bendikj@gmail.com>
2021-08-31 19:57:34 +0200

    Improve seeker script and aim for above head position

--- Common/Characters/Mobs/Seeker.gd
+++ Common/Characters/Mobs/Seeker.gd
@@ -2,8 +2,11 @@ extends "res://Common/Characters/character.gd"
 
 export(Resource) var target_type = preload("res://Common/Characters/Player.gd")
 
-const ACCELERATION = 1.5
-const MAX_ACCELERATION = 670
+
+const VELOCITY = 550
+const CLOSE_DISTANCE = 50
+const POSITION_ABOVE_PLAYER = 50
+const SPEED_TO_TOP_POSITION = 100
 
 onready var animation = $AnimatedSprite
 
@@ -14,19 +17,20 @@ var has_cycled_attack = false
 
 
 func _on_AutoFollow_target_found(collision_point: Vector2, target):
-	target_position = collision_point - global_position
+	target_position = Vector2(target.position.x, target.position.y - POSITION_ABOVE_PLAYER)
 	target_node = target
 
 
 func _on_AutoFollow_target_lost():
 	target_position = Vector2.ZERO
 
-func _process(_delta):
+
+func _physics_process(_delta):
+	var target_direction = target_position - global_position
 	if target_position != Vector2.ZERO and is_instance_valid(target_node):
-		var distance = target_position.length()
-		if distance < 50:
-			var head_position = Vector2(target_position.x, target_position.y - 30.0)
-			velocity = move_and_slide(head_position * 100)
+		var distance = target_direction.length()
+		if distance < CLOSE_DISTANCE:
+			position = position.move_toward(target_position, 10)
 			animation.play("Shock")
 			
 			if animation.frame == 3 and not has_cycled_attack:
@@ -36,12 +40,12 @@ func _process(_delta):
 				has_cycled_attack = false
 		else:
 			animation.play("Idle")
-			var relative_speed = (MAX_ACCELERATION - distance) * ACCELERATION
-			velocity = move_and_slide(target_position * relative_speed * _delta)
+			velocity = move_and_slide(target_direction.normalized() * VELOCITY)
 	else:
 		animation.play("Idle")
 		velocity = Vector2.ZERO
-		
+
+
 func _attack_target():
 	if target_node != null and target_node.has_method("suffer_damage"):
 		target_node.suffer_damage(1)
